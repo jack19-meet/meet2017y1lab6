01939929ddf6af0ff9ff591e7d1a69644c1655bb
jack19-meet <jack19@meet.mit.edu>
2017-07-31 18:28:54 +0300

    Stopping here

--- turtlefun.py
+++ turtlefun.py
@@ -1,5 +1,4 @@
-
-
+import turtle
 
 UP_ARROW = "Up"
 LEFT_ARROW = "Left"
@@ -14,5 +13,41 @@ RIGHT = 3
 
 direction = UP
 
-def up(
-    
+def up():
+    global direction
+    direction = UP
+    print (direction)
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y-10)
+    print(turtle.pos)
+
+
+def left():
+    global direction
+    direction = LEFT
+    print (direction)
+    old_pos=turtle.pos()
+    x=old_pos[0]
+    y=old_pos[1]
+    turtle.goto(x,y-10)
+    print(turtle.pos)
+
+
+def right():
+    global direction
+    direction = RIGHT
+    print ('you pressed RIGHT')
+
+def down():
+    global direction
+    direction = DOWN
+    print ('you pressed DOWN')
+
+turtle.onkeypress(up,UP_ARROW)
+turtle.onkeypress(left,LEFT_ARROW)
+turtle.onkeypress(right,RIGHT_ARROW)
+turtle.onkeypress(down,DOWN_ARROW)
+
+turtle.mainloop()
